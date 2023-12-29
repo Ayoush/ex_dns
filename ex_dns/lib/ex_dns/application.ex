1cb188e99e8d406b4e8d4c772083e59d1378e363
Ayoush <ayoushchourasia1418@gmail.com>
2023-12-29 11:15:25 +0530

    First draft of the ex_dns
    Created a supervisor project

---
+++ ex_dns/lib/ex_dns/application.ex
@@ -0,0 +1,20 @@
+defmodule ExDns.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: ExDns.Worker.start_link(arg)
+      # {ExDns.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ExDns.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ ex_dns/test/ex_dns_test.exs
@@ -0,0 +1,8 @@
+defmodule ExDnsTest do
+  use ExUnit.Case
+  doctest ExDns
+
+  test "greets the world" do
+    assert ExDns.hello() == :world
+  end
+end
