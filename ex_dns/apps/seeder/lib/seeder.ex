c9221e087d122d4681739e859c0c390fbf0e8235
Ayoush <ayoushchourasia1418@gmail.com>
2025-04-16 20:29:47 +0530

    Resetted the code and added something

---
+++ ex_dns/apps/seeder/lib/seeder.ex
@@ -0,0 +1,18 @@
+defmodule Seeder do
+  @moduledoc """
+  Documentation for `Seeder`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Seeder.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
